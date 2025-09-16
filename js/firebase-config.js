63868cc4e6516ad0268739f041538f5e63dd75c6
apd2019 <apd102018@gmail.com>
2025-09-17 01:25:25 +0530

    first-commit

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// contains Firebase credentials and setup
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqg_BfCD4NlDqFC4doihRA_I0Jn44Osf8",
+    authDomain: "simply-blog-5a978.firebaseapp.com",
+    projectId: "simply-blog-5a978",
+    storageBucket: "simply-blog-5a978.firebasestorage.app",
+    messagingSenderId: "404206482749",
+    appId: "1:404206482749:web:6c978f683703e28a4d8154",
+    measurementId: "G-BNLRPS9DL4"
+  };
+
+  firebase.initializeApp(firebaseConfig);
---
+++ navbar.js
@@ -0,0 +1,10 @@
+document.getElementById("navbar").innerHTML = `
+ <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="login.html">Login</a>
+    <a href="register.html">Sign Up</a>
+    <a href="contact.html">Contact</a>
+    <a href="blogs.html">Blogs</a>
+ </nav>
+`;
